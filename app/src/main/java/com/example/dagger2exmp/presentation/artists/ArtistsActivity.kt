395b0a59d2c665d6f18e0bb35ff366e0614edf39
Ashfaquer_Rahmani <ashfaqueakhtar017@gmail.com>
2023-08-28 13:02:53 +0530

    Artists List Loaded

--- app/src/main/java/com/example/dagger2exmp/presentation/artists/ArtistsActivity.kt
+++ app/src/main/java/com/example/dagger2exmp/presentation/artists/ArtistsActivity.kt
@@ -2,17 +2,57 @@ package com.example.dagger2exmp.presentation.artists
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dagger2exmp.R
 import com.example.dagger2exmp.databinding.ActivityArtistsBinding
+import com.example.dagger2exmp.presentation.di.Injector
+import javax.inject.Inject
 
 class ArtistsActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: ArtistViewModelFactory
+
+    private lateinit var viewModel: ArtistViewModel
+
     private lateinit var binding: ActivityArtistsBinding
+
+    private val adapter = ArtistsAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_artists)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_artists)
+
+        (application as Injector).createArtistSubComponent().inject(this)
+
+        viewModel = ViewModelProvider(this, factory)[ArtistViewModel::class.java]
+
+        initView()
+    }
+
+    private fun initView() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+        displayPopularArtists()
+    }
+
+    private fun displayPopularArtists() {
+        binding.progressBar.visibility = View.VISIBLE
+        viewModel.getArtists().observe(this, Observer {
+            it?.let { list ->
+                binding.progressBar.visibility = View.GONE
+                adapter.setList(list)
+            } ?: run {
+                binding.progressBar.visibility = View.GONE
+                Toast.makeText(applicationContext, "No Data found", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
--- app/src/main/java/com/example/dagger2exmp/presentation/artists/ArtistsAdapter.kt
+++ app/src/main/java/com/example/dagger2exmp/presentation/artists/ArtistsAdapter.kt
@@ -1,7 +1,48 @@
 package com.example.dagger2exmp.presentation.artists
 
-class ArtistsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.dagger2exmp.R
+import com.example.dagger2exmp.data.model.artist.Person
+import com.example.dagger2exmp.databinding.ListItemBinding
 
+class ArtistsAdapter : RecyclerView.Adapter<ArtistsAdapter.MyViewHolder>() {
 
-    inner class  MyViewHolder()
+    private var mList = ArrayList<Person>()
+
+    fun setList(list: List<Person>) {
+        mList.clear()
+        mList.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    inner class MyViewHolder(private val binding: ListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(person: Person) {
+            binding.titleTextView.text = person.name
+            binding.descriptionTextView.text = person.knownForDepartment
+            val posterUrl = "https://image.tmdb.org/t/p/w500/" + person.profilePath
+            Glide.with(binding.imageView.context)
+                .load(posterUrl)
+                .into(binding.imageView)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtistsAdapter.MyViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding: ListItemBinding =
+            DataBindingUtil.inflate(inflater, R.layout.list_item, parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    override fun onBindViewHolder(holder: ArtistsAdapter.MyViewHolder, position: Int) {
+        holder.bind(mList[position])
+    }
 }
